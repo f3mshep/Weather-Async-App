b56499d3ac3f023379d452dd5e16ecd62aab7a04
f3mshep <superbiscuit@gmail.com>
2018-05-07 12:17:22 -0700

    weather cli works

--- app.js
+++ app.js
@@ -24,17 +24,14 @@ geocode.geocodeAddress(argv.address, (errorMessage, results)=>{
     console.log(errorMessage);
   }
   else {
-    console.log(JSON.stringify(results, undefined, 2));
-  }
-});
-
-weather.getWeather(37.9598787, -122.1017303, (error, results)=>{
-  if(error){
-    console.log("Unable to fetch the weather")
-  }
-  else{
-    console.log(`Temperature: ${results.currently.temperature}`);
-    console.log(`Chance of Rain: %${results.currently.precipProbability}`);
-    console.log(`Weather: ${results.minutely.summary}`);
+    weather.getWeather(results.latitude, results.longitude, (error, results) => {
+      if (error) {
+        console.log("Unable to fetch the weather");
+      } else {
+        console.log(`Temperature: ${results.currently.temperature}`);
+        console.log(`Chance of Rain: %${results.currently.precipProbability}`);
+        console.log(`Weather: ${results.minutely.summary}`);
+      }
+    });
   }
 });
